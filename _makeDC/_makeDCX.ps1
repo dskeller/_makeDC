22965e782214849dd60a9a5c0545766f14ec9ba0
dskeller <davidsebastian.keller@gmail.com>
2021-08-31 15:56:28 +0200

    Initial commit

---
+++ _makeDC/_makeDCX.ps1
@@ -0,0 +1,43 @@
+[cmdletbinding()]
+param(
+  [System.IO.file]$configfile
+)
+
+#import configfile
+[xml]$config = Get-Content -Path "$configfile"
+
+#validate config
+#TODO
+
+#set netipaddress
+
+# get network adapter / including case netadapter>1
+if ($((Get-NetAdapter | Measure-Object).Count) -lt 1){
+  throw "No network adapter found"
+}
+elseif ($((Get-NetAdapter | Measure-Object).Count) -gt 1){
+  $netadapter = Read-Host -prompt "Please specifiy the name of the network adapter to use"
+  if (-not(Get-NetAdapter -Name $netadapter)){
+    throw "No network adapter found with name '$netadapter'"
+  }
+}else{
+  $netadapter = (Get-NetAdapter).Name
+}
+
+# disable DHCP for network adapter
+Set-NetIPInterface -InterfaceAlias "$netadapter" -AddressFamily IPv4 -DHCP Disabled -PassThru
+ 
+# set ip, subnet and gateway
+New-NetIPAddress -AddressFamily IPv4 -InterfaceAlias "$netadapter" -IPAddress $($config.config.ipaddress) -PrefixLength $($config.config.subnetmask) -DefaultGateway $($config.config.gateway)
+ 
+# set dns to server
+Set-DnsClientServerAddress -InterfaceAlias "$netadapter" -ServerAddresses $($config.config.ipaddress)
+ 
+# disable ipv6
+#Disable-NetAdapterBinding -Name "$netadapter" -ComponentID ms_tcpip6
+
+# install roles and features
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
+
+# configure adds
+Install-ADDSDomainController -Credential (Get-Credential Nocksoft\Administrator) -DomainName $($config.config.domainName) -SkipPreChecks -NoGlobalCatalog:$true -CriticalReplicationOnly:$false -InstallDns:$true -SiteName "Default-First-Site-Name" -SafeModeAdministratorPassword $(ConvertTo-SecureString $($config.config.smAdmPwd) -AsPlaintext -Force) -Force
